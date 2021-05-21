38adef1d6792d3a64433d23f3cf7ef8d2ccf2772
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-21 23:20:35 +0300

    Create What1.py

---
+++ What1.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+import random
+import sys
+
+os.system("clear")
+print("# \033[1;34m[ 1 ] >> \033[1;36;40mTermux")
+print("# \033[1;34m[ 2 ] >> \033[1;36;40mLinux")
+print("# \033[1;34m[ 3 ] >> \033[1;36;40mOther")
+
+op=int(raw_input("Options: "))
+
+elif(op==1):
+ os.system("cd")
+ os.system("cd Free-Proxy")
+ os.system("bash Setup.sh")
+elif(op==2):
+ os.system("cd")
+ os.system("cd Free-Proxy")
+ os.system("bash SetupLinux.sh")
+elif(op==3)
+ os.system("cd")
+ os.system("cd Free-Proxy && cd packages")
+ os.system("bash OtherInstall.sh")
