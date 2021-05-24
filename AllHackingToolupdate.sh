88282c55b6890f8372928165451cc5376cb62ff8
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-24 20:28:16 +0300

    Create AllHackingToolupdate.sh

---
+++ AllHackingToolupdate.sh
@@ -0,0 +1,56 @@
+echo "██╗░░░██╗██████╗░██████╗░░█████╗░████████╗██╗███╗░░██╗░██████╗░"
+echo "██║░░░██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║████╗░██║██╔════╝░"
+echo "██║░░░██║██████╔╝██║░░██║███████║░░░██║░░░██║██╔██╗██║██║░░██╗░"
+echo "██║░░░██║██╔═══╝░██║░░██║██╔══██║░░░██║░░░██║██║╚████║██║░░╚██╗"
+echo "╚██████╔╝██║░░░░░██████╔╝██║░░██║░░░██║░░░██║██║░╚███║╚██████╔╝"
+echo "░╚═════╝░╚═╝░░░░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░"
+cd 
+cd
+rm -rf AllHackingTools
+git clone https://github.com/mishakorzik/AllHackingToolscd AutoUpdateMyTools
+python2 What1.py
+cd 
+cd
+cd AllHackingTools
+apt-get install git 
+apt-get install python
+apt-get install wget 
+apt-get install openssh 
+apt-get install php
+apt-get install toilet 
+apt-get install colorama
+apt-get install jq 
+apt-get install apache2
+apt-get install ruby 
+apt-get install ssl
+apt-get install w3m 
+apt-get install figlet
+apt-get install zip 
+apt-get install lolcat
+apt-get install pv 
+apt-get install neofetch
+apt-get install zsh
+sleep 1
+echo "██████╗░░█████╗░░██████╗███████╗   ██╗███╗░░██╗░██████╗████████╗░█████╗░██╗░░░░░██╗░░░░░██╗███╗░░██╗░██████╗░" 
+echo "██╔══██╗██╔══██╗██╔════╝██╔════╝   ██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██║░░░░░██║░░░░░██║████╗░██║██╔════╝░"
+echo "██████╦╝███████║╚█████╗░█████╗░░   ██║██╔██╗██║╚█████╗░░░░██║░░░███████║██║░░░░░██║░░░░░██║██╔██╗██║██║░░██╗░"
+echo "██╔══██╗██╔══██║░╚═══██╗██╔══╝░░   ██║██║╚████║░╚═══██╗░░░██║░░░██╔══██║██║░░░░░██║░░░░░██║██║╚████║██║░░╚██╗"
+echo "██████╦╝██║░░██║██████╔╝███████╗   ██║██║░╚███║██████╔╝░░░██║░░░██║░░██║███████╗███████╗██║██║░╚███║╚██████╔╝"
+echo "╚═════╝░╚═╝░░╚═╝╚═════╝░╚══════╝   ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝╚═╝░░╚══╝░╚═════╝░"
+sleep 2
+apt-get install pip
+apt-get install pip2
+apt-get install pip3
+apt-get install curl
+sleep 1
+cd 
+cd AllHackingTools
+echo "██████╗░░█████╗░███╗░░██╗███████╗"
+echo "██╔══██╗██╔══██╗████╗░██║██╔════╝"
+echo "██║░░██║██║░░██║██╔██╗██║█████╗░░"
+echo "██║░░██║██║░░██║██║╚████║██╔══╝░░"
+echo "██████╔╝╚█████╔╝██║░╚███║███████╗"
+echo "╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝"
+echo Developer: mishakorzhik
+echo Create on: 24 05 2021
+echo "PLEASE WRITE THE COMMAND cd"
