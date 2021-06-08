3ecd9accf9528f1b27371928ddae717b798bd29f
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 11:26:19 +0300

    Create UninstallAllHackingTools.sh

---
+++ Files/UninstallAllHackingTools.sh
@@ -0,0 +1,30 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
+sleep 0.2
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
+sleep 0.2
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing.."$w
+sleep 0.2
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
+sleep 0.2
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
+sleep 0.2
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
+sleep 0.2
+clear
+cd
+cd
+rm -rf AllHackingTools
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removed succefull! AllHackingTools has deleted!"$w
+sleep 1
+
