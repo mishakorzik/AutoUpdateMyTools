aee6b92b13cdc30bd14a0d595346ae69a8d1d89e
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-08 22:58:34 +0300

    Update UninstallAllHackingTools.sh

--- Files/UninstallAllHackingTools.sh
+++ Files/UninstallAllHackingTools.sh
@@ -5,25 +5,26 @@ w="\033[0m"
 o="\033[1;33m"
 
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
-sleep 0.2
+sleep 0.7
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
-sleep 0.2
+sleep 0.7
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing.."$w
-sleep 0.2
+sleep 0.7
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
-sleep 0.2
+sleep 0.7
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
-sleep 0.2
+sleep 0.7
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
-sleep 0.2
+sleep 0.7
 clear
 cd
 cd
+rm -rf msdconsole.sh
 rm -rf AllHackingTools
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removed succefull! AllHackingTools has deleted!"$w
 sleep 1
