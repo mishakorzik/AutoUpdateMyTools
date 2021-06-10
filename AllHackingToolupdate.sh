9885c15d5a16a24dd8756b9e8bc1d2e1a8725cd4
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-10 17:52:18 +0300

    Update AllHackingToolupdate.sh

--- AllHackingToolupdate.sh
+++ AllHackingToolupdate.sh
@@ -17,15 +17,8 @@ cd
 sleep 1
 cd AllHackingTools
 bash Install.sh
-echo "██████╗░░█████╗░███╗░░██╗███████╗"
-echo "██╔══██╗██╔══██╗████╗░██║██╔════╝"
-echo "██║░░██║██║░░██║██╔██╗██║█████╗░░"
-echo "██║░░██║██║░░██║██║╚████║██╔══╝░░"
-echo "██████╔╝╚█████╔╝██║░╚███║███████╗"
-echo "╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝"
-echo Developer: mishakorzhik
-echo Create on: 24 05 2021
-sleep 2 
+sleep 2
 cd  
+cd
 cd AllHackingTools
-bash AllHackingTool.sh
+bash src/AllHackingTool.sh
