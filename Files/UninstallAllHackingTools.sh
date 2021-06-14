0c6be582dd6b5d9cb968a2bfc95bc55ed2e12a06
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-14 17:28:53 +0300

    Update UninstallAllHackingTools.sh

--- Files/UninstallAllHackingTools.sh
+++ Files/UninstallAllHackingTools.sh
@@ -5,22 +5,37 @@ w="\033[0m"
 o="\033[1;33m"
 
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
-sleep 0.7
+sleep 0.8
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
-sleep 0.7
+sleep 0.8
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing.."$w
-sleep 0.7
+sleep 0.8
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
-sleep 0.7
+sleep 0.8
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
-sleep 0.7
+sleep 0.8
 clear
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
-sleep 0.7
+sleep 0.8
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
+sleep 0.8
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing.."$w
+sleep 0.8
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
+sleep 0.8
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing."$w
+sleep 0.8
+clear
+echo -e $w"["$o"SYSTEM"$w"]"$b"Removing..."$w
+sleep 0.8
 clear
 cd
 cd
@@ -30,8 +45,9 @@ bash remove.sh
 clear
 cd
 cd
-rm -rf msdconsole
 rm -rf AllHackingTools
+cd cd /data/data/com.termux/files/usr/bin/
+rm -rf msdconsole
 echo -e $w"["$o"SYSTEM"$w"]"$b"Removed succefull! AllHackingTools has deleted!"$w
 sleep 1
 
