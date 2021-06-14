730aad1bf5ef4fd3902d06a69880a4102891cb84
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-14 11:39:51 +0300

    Update AllHackingToolupdate.sh

--- AllHackingToolupdate.sh
+++ AllHackingToolupdate.sh
@@ -8,6 +8,17 @@ echo '
 '|lolcat -a -d 8 -s 30.0 -p 1.5
 cd 
 cd
+rm -rf msdconsole
+cd AllHackingTools
+cd termux-style
+./uninstall
+cd
+cd
+cd AllHackingTools
+cd Termux-Banner
+bash remove.sh
+cd
+cd
 rm -rf AllHackingTools
 git clone https://github.com/mishakorzik/AllHackingTools
 cd AutoUpdateMyTools
