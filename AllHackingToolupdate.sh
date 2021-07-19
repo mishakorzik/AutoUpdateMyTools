c67c4ca7688784c2e5149f956010e49fd8804992
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-07-19 11:15:33 +0300

    Update AllHackingToolupdate.sh

--- AllHackingToolupdate.sh
+++ AllHackingToolupdate.sh
@@ -8,6 +8,15 @@ echo '
 '|lolcat -a -d 8 -s 30.0 -p 1.5
 cd 
 cd
+cd /data/data/com.termux/files/usr/bin/
+rm -rf msdc
+rm -rf msdconsoleUPD
+rm -rf sys
+rm -rf system
+rm -rf view-deleted-activity
+rm -rf msd
+rm -rf ms
+rm -rf m
 rm -rf msdconsole
 cd AllHackingTools
 cd termux-style
@@ -25,9 +34,4 @@ cd
 sleep 1
 cd AllHackingTools
 bash Install.sh
-sleep 2
-echo "Starting AllHackingTools"
-sleep 3
-cd  
-cd
-msdconsole
+sleep 1
